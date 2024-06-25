4e215ad44303435d9f2cde9b24cc873d30bf1bbe
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-06-25 21:36:31 +0530

    one more try

--- app.js
+++ app.js
@@ -20,9 +20,13 @@ app.use(
     saveUninitialized: false,
 
     cookie: {
-      secure: process.env.NODE_ENV === "development" ? false : true,
-      httpOnly: process.env.NODE_ENV === "development" ? false : true,
-      sameSite: process.env.NODE_ENV === "development" ? false : "none",
+      //change it back after experimenting
+      // secure: process.env.NODE_ENV === "development" ? false : true,
+      // httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      // sameSite: process.env.NODE_ENV === "development" ? false : "none",
+      secure: false,
+      httpOnly: false,
+      sameSite: "none",
     },
   })
 );
