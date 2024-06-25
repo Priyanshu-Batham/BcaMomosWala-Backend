87bef1bec4becb2845a1f98e22bfb6a23ef84047
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-06-25 19:03:44 +0530

    please work this time

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,10 @@ router.get(
 router.get(
   "/login",
   passport.authenticate("google", {
-    successRedirect: process.env.FRONTEND_URL,
+    // successRedirect: process.env.FRONTEND_URL,
+    failureRedirect: '/googlelogin',
+  }, (req, res)=>{
+    res.redirect(process.env.FRONTEND_URL);
   })
 );
 
