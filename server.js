be8afad0479767cde99d3511c62a3c40b0a7e3c5
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-06-25 21:54:30 +0530

    doing smthn crazy

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 
 app.listen(process.env.PORT, () =>
-  console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
+  console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode. Google Callback url = ${process.env.GOOGLE_CLIENT_URL}`)
 );
