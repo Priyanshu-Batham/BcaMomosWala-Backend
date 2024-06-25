4ac33cb09c758dbb2eea82841a18b8d645a6e9fa
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-06-25 17:30:37 +0530

    specified origins for my cors

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000", "https://bcamomoswala.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
